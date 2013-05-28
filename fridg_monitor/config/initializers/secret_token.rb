55a8bc95cca1a75d4d5574fd344532523c1fa406
Dan Seely <dan@danseely.net>
2013-05-28 19:41:47 -0400

    first commit

---
+++ fridg_monitor/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FridgMonitor::Application.config.secret_token = 'd70c27ed4e7955147b243b88b3e38b2e0edc6f9270c381f0c8dfe728beb1aac6bce55246e80b8fdca3c6a1a1c1f307595ea2be7f6fda173f5154e70ff23db27f'
